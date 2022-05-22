d5faa38bd401558b84eeaf2f9361ea367ef17e08
RathishAadiarja <rathishaadi2000@gmail.com>
2022-05-22 13:23:02 +0200

    removed unwanted functionality

--- random_songs.py
+++ random_songs.py
@@ -3,8 +3,6 @@ import aiohttp
 import random 
 
 # default values
-ASCII_ALPHABET_LOWER_MIN = 97
-ASCII_ALPHABET_LOWER_MAX = 122
 MAX_FETCH_LIMIT = 5
 MIN_RANDOM_WORDS_NUM = 5
 MAX_RANDOM_WORDS_NUM = 20
@@ -24,7 +22,7 @@ class GetRandomSongs():
         }                                            
 
     def get_number_of_words_from_user(self) -> int:
-        # gets input number from user
+
         # if the number is invalid or not an integer, the minimum number of words to fetch (5) is considered as default value
         try:
             print("Wanna know some random words and the songs associated with those words?")
@@ -39,10 +37,12 @@ class GetRandomSongs():
             return self.number_of_words
 
     def get_words_sessions(self, session, fetch_nums) -> list:
+
         # type of session - <class 'aiohttp.client.ClientSession'>
         # fetch_nums - number of words to fetch
+
         session_list = []
-        print(type(session))
+
         for i in range(fetch_nums):
             session_list.append(session.get(RANDOM_WORDS_URL, ssl=False))
         return session_list
@@ -64,11 +64,10 @@ class GetRandomSongs():
 
                 # the default response is a json format of list/array containing a single dictionary/object  
                 # if the response is not in a valid format(error response)
-                # example resultant string from the get request  : 'Something Went Wrong - Enter the Correct API URL'
+                # example resultant string from the get request: 'Something Went Wrong - Enter the Correct API URL'
                 # the response is skipped by checking the length of the response(default length = 1)
                 
-                temp_random_words = [i[0]['word'] for i in temp_random_words if len(i)==1]
-                temp_random_words = [i.lower() for i in temp_random_words]
+                temp_random_words = [i[0]['word'].lower() for i in temp_random_words if len(i)==1]
 
                 # only unique words are added to self.random_words
                 # the variable 'duplicate_len' represents number of unique words more to be fetched
@@ -78,40 +77,39 @@ class GetRandomSongs():
 
                 if  duplicate_len > 0:
                     number_of_words_to_fetch = duplicate_len
-                elif duplicate_len > 0 and i == MAX_FETCH_LIMIT-1:
-                    # if there are duplicate words even after reaching MAX_FETCH_LIMIT
-                    # random unique lowercase alphabets are added to the random words list
-                    # so random songs can be fetched atleast from these alphabets
-
-                    alphabet_list = list(random.sample(range(ASCII_ALPHABET_LOWER_MIN, ASCII_ALPHABET_LOWER_MAX+1), duplicate_len))
-                    self.random_words += [chr(i) for i in alphabet_list]
                 else:
                     break
 
         self.random_words = sorted(self.random_words)   
-        print(self.random_words)
 
     def get_songs_sessions(self, session, words_to_fetch) -> list:
+
         # type of session - <class 'aiohttp.client.ClientSession'>
         # words_to_fetch - list of random words to fetch
+
         session_list = []
 
         for word in words_to_fetch:
+
             # the MAX_FETCH_LIMIT is the limit value to misicbranz API
             # limits the number of recordings fetched for an unique word
+
             session_list.append(session.get(MUSICBRAINZ_URL.format(word, MAX_FETCH_LIMIT), ssl=False))
         return session_list
     
     async def fetch_random_songs(self) -> None:
+
         async with aiohttp.ClientSession() as session:      
             url_list = self.get_songs_sessions(session, self.random_words)
             responses = await asyncio.gather(*url_list)
 
             for res in responses: 
                 temp_json = await res.json()
+
                 # if the maximum fetch limit reached for musicbrainz API it returns a dictionary/object with key 'error'
                 # {'error': 'Your requests are exceeding the allowable rate limit. Please see http://wiki.musicbrainz.org/XMLWebService for more information.'}
                 # then for the subsequent random words the values of title, artist and album are set to None 
+
                 if 'error' in temp_json.keys():
                     total_recordings = 0
                 else:
@@ -119,13 +117,13 @@ class GetRandomSongs():
 
                 temp_dict = {}
                 if(total_recordings > 0):
-                    top_song = top_artist = top_release = 0
+                    top_song = 0
                     max_recordings_iter = total_recordings if total_recordings < MAX_FETCH_LIMIT else MAX_FETCH_LIMIT
 
                     for i in range(max_recordings_iter):
                         temp_dict['title'] = temp_json['recordings'][top_song]['title']
-                        temp_dict['artist'] =  temp_json['recordings'][top_song]['artist-credit'][top_artist]['name']
-                        temp_dict['album'] = temp_json['recordings'][top_song]['releases'][top_release]['title']
+                        temp_dict['artist'] =  temp_json['recordings'][top_song]['artist-credit'][0]['name']
+                        temp_dict['album'] = temp_json['recordings'][top_song]['releases'][0]['title']
 
                         if temp_dict in self.random_songs:
                             top_song += 1
@@ -137,7 +135,7 @@ class GetRandomSongs():
                 self.random_songs.append(temp_dict)
      
     def make_words_and_songs_dict(self):
-        # putting the random words and songs together in a dictionary
+        
         self.random_words_and_songs_dict['data'] = []
         if len(self.random_words) != len(self.random_songs):
             self.random_words = self.random_words[:len(self.random_songs)]
@@ -167,8 +165,10 @@ class GetRandomSongs():
 
 
     def run_get_words_and_songs(self) -> dict:
+
         # using WindowsSelectorEventLoopPolicy() to override the 'RuntimeError: Event loop is closed' on windows
         asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
         asyncio.run(self.fetch_random_words())
         asyncio.run(self.fetch_random_songs())
         self.make_words_and_songs_dict()
